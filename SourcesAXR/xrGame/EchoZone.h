397a1b161fc42b1053da67f3f2cb426ac193fa6b
mnelenpridumivat <ragrind@mail.ru>
2024-06-25 15:14:22 +0300

    Minor addition

--- SourcesAXR/xrGame/EchoZone.h
+++ SourcesAXR/xrGame/EchoZone.h
@@ -8,8 +8,12 @@ class CEchoZone : public CCustomZone, public Feel::Sound//, public CExplosive
 {
 	typedef	CCustomZone	inherited;
 
+protected:
+
+	float DamageMax = 0.0f;
+
 public:
 	void feel_sound_new(CObject* who, int type, CSound_UserDataPtr user_data, const Fvector& Position, float power) override;
 	void Load(LPCSTR section) override;
-	Feel::Sound* dcast_FeelSound() override { return this; };
+	Feel::Sound* dcast_FeelSound() override { return this; }
 };
